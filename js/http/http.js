86dd303013e44553778d54e8c1317eaae8aed141
Эржигит Имамов <erjigit17@gmail.com>
2021-09-10 18:07:14 +0600

    add http

--- js/http/http.js
+++ js/http/http.js
@@ -1,2 +1,29 @@
 'use strict'
 
+const fs = require('fs')
+const zlib = require('zlib')
+const http = require('http')
+
+const rs = fs.createReadStream('index.html')
+const gs = zlib.createBrotliCompress()
+
+const buffers = []
+let buffer = null
+
+gs.on('data', (buffer) => {
+  buffers.push(buffer)
+})
+
+gs.on('end', () => {
+  buffer = Buffer.concat(buffers)
+})
+
+rs.pipe(gs)
+
+const server = http.createServer((req, res) =>{
+  console.log(req.url)
+  res.writeHead(200, {'Content-Encoding': 'br'})
+  res.end(buffer)
+})
+
+server.listen(3000)
